c440fa854f7247c825366c556c10eaee355d98f8
Jesse Sanders <jesse.sanders@briebug.com>
2018-08-02 16:21:46 -0600

    project init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { ProductsComponent } from "./products/products.component";
+import { Sidebar2Component } from "./sidebar2/sidebar2.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "",
+    component: DashboardComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "products",
+    component: ProductsComponent
+  },
+  {
+    path: "sidebar",
+    component: SidebarComponent,
+    outlet: "sidebar"
+  },
+  {
+    path: "",
+    component: SidebarComponent,
+    outlet: "sidebar",
+    pathMatch: "full"
+  },
+  {
+    path: "sidebar2",
+    component: Sidebar2Component,
+    outlet: "sidebar"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { ProductsComponent } from "./products/products.component";
+import { Sidebar2Component } from "./sidebar2/sidebar2.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    ProductsComponent,
+    Sidebar2Component
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
